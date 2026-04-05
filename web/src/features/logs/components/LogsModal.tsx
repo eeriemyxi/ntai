f5c4c432144ee923d6f845e164efc1d92c6075e5
eeriemyxi <myxi@envs.net>
2026-04-05 13:21:36 +0530

    feat: make the logs modal responsive (work on phones)

--- web/src/features/logs/components/LogsModal.tsx
+++ web/src/features/logs/components/LogsModal.tsx
@@ -1,23 +1,28 @@
 import {
   Button,
-  Stack,
   Image,
   Paper,
   ScrollArea,
   SimpleGrid,
+  Stack,
+  useMantineTheme,
 } from "@mantine/core";
 import { modals } from "@mantine/modals";
 import { Logs } from "lucide-react";
 import { useShallow } from "zustand/react/shallow";
+import { useMediaQuery } from '@mantine/hooks';
 
 import { bookUrl, thumbnailUrl } from "@/fetching";
-import classes from './Styles.module.css';
+
 import { useLogsStore } from "../";
+import classes from "./Styles.module.css";
 
 export function LogsModal() {
   const logStore = useLogsStore(
     useShallow((state) => ({ items: state.items, logItem: state.logItem })),
   );
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
 
   return (
     <Button
@@ -28,13 +33,20 @@ export function LogsModal() {
       onClick={() =>
         modals.open({
           title: "History",
-          size: "70%",
+          size: "90%",
+          fullScreen: isMobile,
           scrollAreaComponent: (props) => (
-            <ScrollArea.Autosize {...props} type="scroll" scrollbarSize={6} scrollHideDelay={100} offsetScrollbars />
+            <ScrollArea.Autosize
+              {...props}
+              type="scroll"
+              scrollbarSize={6}
+              scrollHideDelay={100}
+              offsetScrollbars
+            />
           ),
           children: (
             <>
-              <SimpleGrid cols={5}>
+              <SimpleGrid cols={{base: 1, sm: 5}}>
                 {Object.values(logStore.items).map((model, index) => (
                   <Paper
                     key={index}
@@ -43,8 +55,7 @@ export function LogsModal() {
                     href={bookUrl(model)}
                     target="_blank"
                     className={classes.nativeMantineFeel}
-                    p="md"
-                  >
+                    p="md">
                     <Stack>
                       <Image
                         src={thumbnailUrl(model)}
@@ -61,8 +72,7 @@ export function LogsModal() {
           ),
         })
       }
-      leftSection={<Logs size={16} />}
-    >
+      leftSection={<Logs size={16} />}>
       Logs
     </Button>
   );
