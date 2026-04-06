cc377ca17794872308c144bf5bc525724dad4cdc
eeriemyxi <myxi@envs.net>
2026-04-07 01:22:35 +0530

    fix: issues with package name

--- src/ntai/__init__.py
+++ src/ntai/__init__.py
@@ -1,5 +1,4 @@
-from importlib.metadata import metadata
 from importlib.resources import files
 
-PACKAGE_NAME = metadata(__package__ or "")["Name"]
+PACKAGE_NAME = "ntai"
 SERVE_DIR = files(f"{PACKAGE_NAME}.data") / "serve"
